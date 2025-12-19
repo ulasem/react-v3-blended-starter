3da9e4a08615f7c30e3cec170a382eed8e473d12
Yuliia <semockoula@gmail.com>
2025-12-19 20:52:43 +0200

    -

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
       }
       setPhotos(fetchedPhotos);
     } catch {
-      setIsError(true);
+      setIsError(false);
     } finally {
       setIsLoading(false);
     }
